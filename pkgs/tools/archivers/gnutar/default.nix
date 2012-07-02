adrianpk/nixpkgs

commit 9786a9826107dd1d7751d06f5d22610ae5283b78

tar, gzip, diffutils: Allow builds with newer versions of glibc.
This is a follow-up to 77fdc7bb3583606c86b2dd602b76a20a1bf6bc7c ("GNU
Coreutils: Allow builds with newer versions of glibc.").

pkgs/tools/archivers/gnutar/default.nix
@@ -37,4 +37,14 @@ stdenv.mkDerivation (rec {
   # cannot be used as a login shell for now.
 // stdenv.lib.optionalAttrs (stdenv.system == "armv7l-linux") {
   FORCE_UNSAFE_CONFIGURE = 1;
+}
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
 })

pkgs/tools/compression/gzip/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (rec {
   name = "gzip-1.4";
 
   src = fetchurl {
@@ -31,3 +31,13 @@ stdenv.mkDerivation rec {
     maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
+})

pkgs/tools/text/diffutils/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchurl, coreutils ? null}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation ({
   name = "diffutils-3.2";
   
   src = fetchurl {
@@ -16,3 +16,13 @@ stdenv.mkDerivation {
     description = "Commands for showing the differences between files (diff, cmp, etc.)";
   };
 }
+
+//
+
+{
+  crossAttrs =
+    # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
+    (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
+      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+    });
+})

commit 2b171246ef57a28e57af49526a5d8470e42c3574

tar, gzip: Adjust the `gets' patch.
Fixes the incorrect patches introduced in
9786a9826107dd1d7751d06f5d22610ae5283b78.

pkgs/tools/archivers/gnutar/default.nix
@@ -45,6 +45,6 @@ stdenv.mkDerivation (rec {
   crossAttrs =
     # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
     (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
-      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+      patches = [ ./gets-undeclared.patch ];
     });
 })

pkgs/tools/archivers/gnutar/gets-undeclared.patch
@@ -1,0 +1,26 @@
+This patch is needed to allow builds with newer versions of
+the GNU libc (2.16+).
+
+This is a backport of this patch:
+
+commit 66712c23388e93e5c518ebc8515140fa0c807348
+Author: Eric Blake <eblake@redhat.com>
+Date:   Thu Mar 29 13:30:41 2012 -0600
+
+    stdio: don't assume gets any more
+    
+--- tar-1.26/gnu/stdio.in.h	2012-07-02 14:28:45.000000000 +0200
++++ tar-1.26/gnu/stdio.in.h	2012-07-02 14:28:50.000000000 +0200
+@@ -160,12 +160,6 @@ _GL_WARN_ON_USE (fflush, "fflush is not
+                  "use gnulib module fflush for portable POSIX compliance");
+ #endif
+ 
+-/* It is very rare that the developer ever has full control of stdin,
+-   so any use of gets warrants an unconditional warning.  Assume it is
+-   always declared, since it is required by C89.  */
+-#undef gets
+-_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+-
+ #if @GNULIB_FOPEN@
+ # if @REPLACE_FOPEN@
+ #  if !(defined __cplusplus && defined GNULIB_NAMESPACE)

pkgs/tools/compression/gzip/default.nix
@@ -38,6 +38,6 @@ stdenv.mkDerivation (rec {
   crossAttrs =
     # XXX: Temporary workaround to allow GNU/Hurd builds with newer libcs.
     (stdenv.lib.optionalAttrs (stdenv.cross.config == "i586-pc-gnu") {
-      patches = [ ../../misc/coreutils/gets-undeclared.patch ];
+      patches = [ ./gets-undeclared.patch ];
     });
 })

pkgs/tools/compression/gzip/gets-undeclared.patch
@@ -1,0 +1,26 @@
+This patch is needed to allow builds with newer versions of
+the GNU libc (2.16+).
+
+This is a backport of this patch:
+
+commit 66712c23388e93e5c518ebc8515140fa0c807348
+Author: Eric Blake <eblake@redhat.com>
+Date:   Thu Mar 29 13:30:41 2012 -0600
+
+    stdio: don't assume gets any more
+    
+--- gzip/lib/stdio.in.h	2012-07-02 14:28:45.000000000 +0200
++++ gzip/lib/stdio.in.h	2012-07-02 14:28:50.000000000 +0200
+@@ -160,12 +160,6 @@ _GL_WARN_ON_USE (fflush, "fflush is not
+                  "use gnulib module fflush for portable POSIX compliance");
+ #endif
+ 
+-/* It is very rare that the developer ever has full control of stdin,
+-   so any use of gets warrants an unconditional warning.  Assume it is
+-   always declared, since it is required by C89.  */
+-#undef gets
+-_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+-
+ #if @GNULIB_FOPEN@
+ # if @REPLACE_FOPEN@
+ #  if !(defined __cplusplus && defined GNULIB_NAMESPACE)