adrianpk/nixpkgs

commit d7d3c8fd828cf6aba4d511be1927458507422a67

sqliteodbc: update to 0.995, add more metadata
close #1122

pkgs/development/libraries/unixODBCDrivers/default.nix
@@ -78,19 +78,29 @@ args : with args;
       "FileUsage       = 3\n ";
  };
  sqlite = rec {
-    deriv = stdenv.mkDerivation {
-      name = "sqlite-connector-odbc-3.51.12";
+    deriv = let version = "0.995"; in
+    stdenv.mkDerivation {
+      name = "sqlite-connector-odbc-${version}";
+
       src = fetchurl {
-        url = http://www.ch-werner.de/sqliteodbc/sqliteodbc-0.70.tar.gz;
-        sha256 = "0ysyqdqkxqcqxrxgi15cbrzia9z6yalim5c88faad85bwanx4db8";
+        url = "http://www.ch-werner.de/sqliteodbc/sqliteodbc-${version}.tar.gz";
+        sha256 = "1r97fw6xy5w2f8c0ii7blfqfi6salvd3k8wnxpx9wqc1gxk8jnyy";
       };
+
+      buildInputs = [ sqlite ];
+
       configureFlags = "--with-sqlite3=${sqlite} --with-odbc=${unixODBC}";
-      postInstall = ''mkdir lib; mv $out/* lib; mv lib $out'';
-      buildInputs = [libtool zlib sqlite];
+
+      postInstall = ''
+        mkdir -p  $out/lib
+      '';
+
       meta = { 
-        description = "sqlite odbc connector, install using configuration.nix";
-        homepage = http://www.ch-werner.de/sqliteodbc/html/index.html;
-        license = "BSD";
+        description = "ODBC driver for SQLite";
+        homepage = http://www.ch-werner.de/sqliteodbc;
+        license = stdenv.lib.licenses.bsd2;
+        platforms = stdenv.lib.platforms.linux;
+        maintainers = with stdenv.lib.maintainers; [ vlstill ];
       };
     };
     ini =

commit 5424e55f6e796c6a86d1ca62bfd0b0c505100ef7

sqliteodbc: fix library location
Commit d7d3c8fd828cf6aba4d511be1927458507422a67 (sqliteodbc: update to
0.995, add more metadata) mistakenly moved libraries from $out/lib to
$out (or rather stopped moving them from $out to $out/lib).

Move them back to $out/lib where they're expected to be and referred to
by the /etc/odbcinst.ini snippet.

pkgs/development/libraries/unixODBCDrivers/default.nix
@@ -91,8 +91,11 @@ args : with args;
 
       configureFlags = "--with-sqlite3=${sqlite} --with-odbc=${unixODBC}";
 
+      # move libraries to $out/lib where they're expected to be
       postInstall = ''
-        mkdir -p  $out/lib
+        mkdir -p "$out/lib"
+        mv "$out"/*.so "$out/lib"
+        mv "$out"/*.la "$out/lib"
       '';
 
       meta = { 