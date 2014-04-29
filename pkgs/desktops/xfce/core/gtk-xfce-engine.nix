adrianpk/nixpkgs

commit 0b2dd3a12fca27a185f9c5518acb7d73d140e1e5

Xfce: Don't depend on GTK+ 3 by default
Given that Xfce is intended as a light-weight desktop environment,
pulling in two versions of GTK+ by default is not ideal.

pkgs/desktops/xfce/core/gtk-xfce-engine.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, gtk, gtk3 }:
+{ stdenv, fetchurl, pkgconfig, intltool, gtk, enableGtk3Support ? false, gtk3 }:
 
 stdenv.mkDerivation rec {
   p_name  = "gtk-xfce-engine";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   name = "${p_name}-${ver_maj}.${ver_min}";
 
   #TODO: gtk3
-  buildInputs = [ pkgconfig intltool gtk gtk3 ];
+  buildInputs = [ pkgconfig intltool gtk ] ++ stdenv.lib.optional enableGtk3Support gtk;
 
   meta = {
     homepage = http://www.xfce.org/;

commit 11ad5c6c1c55cd31d122723e0320af9b0d965c98

xfce: fix typo introduced in recent commit (s/gtk/gtk3/)
Fix typo introduced in commit 0b2dd3a12fca27a185f9c5518acb7d73d140e1e5
(Xfce: Don't depend on GTK+ 3 by default).

pkgs/desktops/xfce/core/gtk-xfce-engine.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   name = "${p_name}-${ver_maj}.${ver_min}";
 
   #TODO: gtk3
-  buildInputs = [ pkgconfig intltool gtk ] ++ stdenv.lib.optional enableGtk3Support gtk;
+  buildInputs = [ pkgconfig intltool gtk ] ++ stdenv.lib.optional enableGtk3Support gtk3;
 
   meta = {
     homepage = http://www.xfce.org/;