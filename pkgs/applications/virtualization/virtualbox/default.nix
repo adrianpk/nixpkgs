adrianpk/nixpkgs

commit 7ffb1f3bdefe54737f158e83154789ed62f56a98

virtualbox: Update to 5.0.2

pkgs/applications/virtualization/virtualbox/default.nix
@@ -14,7 +14,7 @@ with stdenv.lib;
 let
   buildType = "release";
 
-  version = "5.0.0"; # changes ./guest-additions as well
+  version = "5.0.2"; # changes ./guest-additions as well
 
   forEachModule = action: ''
     for mod in \
@@ -60,7 +60,7 @@ in stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://download.virtualbox.org/virtualbox/${version}/VirtualBox-${version}.tar.bz2";
-    sha256 = "bb71356c8f82012c9b5ae16e12302eb111c71ae7b063ada7688fbfa8aa10c2f7";
+    sha256 = "f290c220d62af2a7fdabb1934c1a0b924b68968a236bb2509bcb507d2c19485e";
   };
 
   buildInputs =

pkgs/applications/virtualization/virtualbox/guest-additions/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://download.virtualbox.org/virtualbox/${version}/VBoxGuestAdditions_${version}.iso";
-    sha256 = "7b61f523db7ba75aebc4c7bb0cae2da92674fa72299e4a006c5c67517f7d786b";
+    sha256 = "1qikn5w6j85fh7ynnfnm4yydii9xb0smv9bvsxnpkpkvkk566h4p";
   };
 
   KERN_DIR = "${kernel.dev}/lib/modules/*/build";

commit 261bff51aa2a63d891ef61580fe1060c089636ba

virtualbox: Fix revision/hash for guest additions.
Regression introduced in 7ffb1f3bdefe54737f158e83154789ed62f56a98.

Also added a small notice so that this hopefully won't happen with
future updates.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/applications/virtualization/virtualbox/default.nix
@@ -14,6 +14,7 @@ with stdenv.lib;
 let
   buildType = "release";
 
+  # Do not forget to update extpackRevision and hash!
   version = "5.0.2"; # changes ./guest-additions as well
 
   forEachModule = action: ''
@@ -35,13 +36,13 @@ let
   '';
 
   # See https://github.com/NixOS/nixpkgs/issues/672 for details
-  extpackRevision = "101573";
+  extpackRevision = "102096";
   extensionPack = requireFile rec {
     name = "Oracle_VM_VirtualBox_Extension_Pack-${version}-${extpackRevision}.vbox-extpack";
     # IMPORTANT: Hash must be base16 encoded because it's used as an input to
     # VBoxExtPackHelperApp!
     # Tip: see http://dlc.sun.com.edgesuite.net/virtualbox/4.3.10/SHA256SUMS
-    sha256 = "c357e36368883df821ed092d261890a95c75e50422b75848c40ad20984086a7a";
+    sha256 = "0c49864ea7ab2be8b95c4495e5825b0e48b8611e1761c1b22b86a3f4bf9201bf";
     message = ''
       In order to use the extension pack, you need to comply with the VirtualBox Personal Use
       and Evaluation License (PUEL) by downloading the related binaries from: