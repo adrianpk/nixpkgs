adrianpk/nixpkgs

commit f646b9295ea7a2332667f5ffeb5bd871e76982e9

Add myself as maintainer for emscripten, emscripten-fastcomp.

pkgs/development/compilers/emscripten-fastcomp/default.nix
@@ -35,11 +35,12 @@ stdenv.mkDerivation {
     make
     cp -a Release/bin $out
   '';
+
   meta = with stdenv.lib; {
     homepage = https://github.com/kripken/emscripten-fastcomp;
     description = "emscripten llvm";
     platforms = platforms.all;
-    maintainers = with maintainers; [ qknight ];
+    maintainers = with maintainers; [ qknight matthewbauer ];
     license = stdenv.lib.licenses.ncsa;
   };
 }

pkgs/development/compilers/emscripten/default.nix
@@ -35,11 +35,12 @@ stdenv.mkDerivation {
     echo "CLOSURE_COMPILER = '${closurecompiler}/share/java/compiler.jar'" >> $out/${appdir}/config
     echo "JAVA = '${jre}/bin/java'" >> $out/${appdir}/config
   '';
+
   meta = with stdenv.lib; {
     homepage = https://github.com/kripken/emscripten;
     description = "An LLVM-to-JavaScript Compiler";
     platforms = platforms.all;
-    maintainers = with maintainers; [ qknight ];
+    maintainers = with maintainers; [ qknight matthewbauer ];
     license = licenses.ncsa;
   };
 }

commit 098273230be0110e9f89d0567033ed5da24101df

Fix nixpkgs eval (caused by 'emscripten' expression)
Fixes this:

 $ nix-env -f . -qa '*' --meta --xml --drv-path --show-trace
 error: while evaluating ‘callPackageWith’ at .../lib/customisation.nix:93:35, called from .../pkgs/top-level/all-packages.nix:1411:24:
 while evaluating ‘makeOverridable’ at .../lib/customisation.nix:54:24, called from .../lib/customisation.nix:97:8:
 undefined variable ‘srcFC’ at .../pkgs/development/compilers/emscripten-fastcomp/default.nix:26:14

Also, "matthewbauer" is not defined in ./lib/maintainers.nix, comment
out.

Caused by f646b9295ea7a2332667f5ffeb5bd871e76982e9 and
d078fe1e9c826a63ad078ca4d5364c967984f393.

pkgs/development/compilers/emscripten-fastcomp/default.nix
@@ -4,7 +4,7 @@ let
   rev = "1.36.4";
 in
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "emscripten-fastcomp-${rev}";
 
   srcFC = fetchFromGitHub {
@@ -40,7 +40,7 @@ stdenv.mkDerivation {
     homepage = https://github.com/kripken/emscripten-fastcomp;
     description = "emscripten llvm";
     platforms = platforms.all;
-    maintainers = with maintainers; [ qknight matthewbauer ];
+    maintainers = with maintainers; [ qknight /*matthewbauer (undefined) */ ];
     license = stdenv.lib.licenses.ncsa;
   };
 }

pkgs/development/compilers/emscripten/default.nix
@@ -40,7 +40,7 @@ stdenv.mkDerivation {
     homepage = https://github.com/kripken/emscripten;
     description = "An LLVM-to-JavaScript Compiler";
     platforms = platforms.all;
-    maintainers = with maintainers; [ qknight matthewbauer ];
+    maintainers = with maintainers; [ qknight /*matthewbauer (undefined) */ ];
     license = licenses.ncsa;
   };
 }