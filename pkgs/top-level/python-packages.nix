adrianpk/nixpkgs

commit 7916a16c8ec3340f85cf5b3e27722f0da6fef141

pythonPackages.Werkzeug: 0.10.4 -> 0.11.10

pkgs/top-level/python-packages.nix
@@ -24663,21 +24663,20 @@ in modules // {
 
 
   werkzeug = buildPythonPackage rec {
-    name = "Werkzeug-0.10.4";
+    name = "Werkzeug-0.11.10";
 
     src = pkgs.fetchurl {
       url = "mirror://pypi/W/Werkzeug/${name}.tar.gz";
-      sha256 = "9d2771e4c89be127bc4bac056ab7ceaf0e0064c723d6b6e195739c3af4fd5c1d";
+      sha256 = "1vpf98k4jp4yhbv2jbyq8dj5fdasrd26rkq34pacs5n7mkxxlr6c";
     };
 
     propagatedBuildInputs = with self; [ itsdangerous ];
-
-    doCheck = false;            # tests fail, not sure why
+    nativeBuildInputs = with self; [ pytest requests ];
 
     meta = {
       homepage = http://werkzeug.pocoo.org/;
       description = "A WSGI utility library for Python";
-      license = "BSD";
+      license = licenses.bsd3;
     };
   };
 

commit 29d543ece51e7943bf7aa8dc01f305396543ee16

pythonPackages.Werkzeug: fix build Python 3.x
that was broken by 7916a16c8ec3340f85cf5b3e27722f0da6fef141

pkgs/top-level/python-packages.nix
@@ -24664,8 +24664,12 @@ in modules // {
       sha256 = "1vpf98k4jp4yhbv2jbyq8dj5fdasrd26rkq34pacs5n7mkxxlr6c";
     };
 
+    LC_ALL = "en_US.UTF-8";
+
     propagatedBuildInputs = with self; [ itsdangerous ];
-    nativeBuildInputs = with self; [ pytest requests ];
+    buildInputs = with self; [ pytest requests2 pkgs.glibcLocales ];
+
+
 
     meta = {
       homepage = http://werkzeug.pocoo.org/;