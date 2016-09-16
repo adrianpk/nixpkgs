adrianpk/nixpkgs

commit 29a53017a41ee0390e3b6028aadf303d04103fb7

ghc-8.0.1: cosmetic fix to the Nix expression to unbreak syntax highlighting

pkgs/development/compilers/ghc/8.0.1.nix
@@ -8,7 +8,7 @@ let
   fetchFilteredPatch = args: fetchurl (args // {
     downloadToTemp = true;
     postFetch = ''
-      ${patchutils}/bin/filterdiff --clean --strip-match=1 -x 'testsuite/*' "$downloadedFile" > "$out"
+      ${patchutils}/bin/filterdiff --clean --strip-match=1 -x '"testsuite/"*' "$downloadedFile" > "$out"
     '';
   });
 in

commit 9ea9a57fe3867ca84693af820542d01a1bd3313d

ghc-8.0.1: Revert "cosmetic fix to the Nix expression to unbreak syntax highlighting"
This reverts commit 29a53017a41ee039, which broke filterdiff according to
https://github.com/NixOS/nixpkgs/issues/18665#issuecomment-247626588.

pkgs/development/compilers/ghc/8.0.1.nix
@@ -8,7 +8,7 @@ let
   fetchFilteredPatch = args: fetchurl (args // {
     downloadToTemp = true;
     postFetch = ''
-      ${patchutils}/bin/filterdiff --clean --strip-match=1 -x '"testsuite/"*' "$downloadedFile" > "$out"
+      ${patchutils}/bin/filterdiff --clean --strip-match=1 -x 'testsuite/*' "$downloadedFile" > "$out"
     '';
   });
 in