adrianpk/nixpkgs

commit 00892b7e4ed2e8eb52227db8e8312a06bd1d6699

generic-builder.nix: Update doHaddock
By default, `ghcjs` haddocks are precluded from being built. I see no reason to disallow building haddocks on projects built with `ghc` and `ghcjs` (someone can correct me here). `HaLVM` currently does not support `haddock` since it's a Stage 1 `GHC`. https://github.com/GaloisInc/HaLVM/blob/master/src/misc/build.mk.in#L20

Currently, building `haddocks` for `ghcjs` projects requires altering the derivation, which doesn't allow users to take advantage of the cache. This change will relieve that.

pkgs/development/haskell-modules/generic-builder.nix
@@ -34,8 +34,7 @@
 , license
 , maintainers ? []
 , doCoverage ? false
-# TODO Do we care about haddock when cross-compiling?
-, doHaddock ? !isCross && (!stdenv.isDarwin || stdenv.lib.versionAtLeast ghc.version "7.8")
+, doHaddock ? (!ghc.isHaLVM or true)
 , passthru ? {}
 , pkgconfigDepends ? [], libraryPkgconfigDepends ? [], executablePkgconfigDepends ? [], testPkgconfigDepends ? [], benchmarkPkgconfigDepends ? []
 , testDepends ? [], testHaskellDepends ? [], testSystemDepends ? []

commit 36d15392d799932c8af7dfac5c9155975ef8d2bf

haskell: fix logic error in generic builder introduced in 00892b7e4ed2e8eb52227db8e8312a06bd1d6699
Closes https://github.com/NixOS/nixpkgs/issues/26760.

pkgs/development/haskell-modules/generic-builder.nix
@@ -34,7 +34,7 @@
 , license
 , maintainers ? []
 , doCoverage ? false
-, doHaddock ? (!ghc.isHaLVM or true)
+, doHaddock ? !(ghc.isHaLVM or false)
 , passthru ? {}
 , pkgconfigDepends ? [], libraryPkgconfigDepends ? [], executablePkgconfigDepends ? [], testPkgconfigDepends ? [], benchmarkPkgconfigDepends ? []
 , testDepends ? [], testHaskellDepends ? [], testSystemDepends ? []