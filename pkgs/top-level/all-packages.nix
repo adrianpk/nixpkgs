adrianpk/nixpkgs

commit e5c24abf3b7f12232b7d2ae08415537d54d75c91

virtualbox: 5.2.0 -> 5.2.2
Upstream changes without issue IDs:

 * User interface: various improvements for high resolution screens
 * User interface: added functionality to duplicate optical and floppy
                   images
 * User interface: various improvements for the virtual media manager
 * VMM: fixed emulation so that Plan 9 guests can start once more (5.1.0
        regression)
 * Storage: fixed regression breaking iSCSI
 * Audio: added HDA support for more exotic guests (e.g. Haiku)
 * Serial: fixed hanging I/O when using named pipes on Windows (5.2.0
           regression)
 * Serial: fixed broken communication with certain devices on Linux
           hosts
 * USB/OHCI: improved behavior so that the controller state after a VM
             reset is closer to the initial state after VM start
 * EFI: fixed HFS+ driver which in rare cases failed to access most
        files on a volume
 * Shared clipboard: fixed hang with OS X host and Linux guest
 * Linux hosts: fixed kernel module compilation and start failures with
                Linux kernel 4.14
 * X11 hosts: better handle WM_CLASS setting
 * Linux guests: fixed kernel module compilation and other problems with
                 Linux kernel 4.14
 * Linux guests: fixed various 5.2.0 regressions
 * Bridged networking: fixed duplicate EtherType in VLAN/priority tags
                       on Linux (5.2.0 regression)

The full changelog including issue IDs can be found at:

https://www.virtualbox.org/wiki/Changelog

Aside from just bumping the version number I also had to strip 3 levels
of the paths included in the guest-additions patches, because the
version was hardcoded in there and the patches still apply as-is.

I've re-added the stripped path using patchFlags and the -d option of
the patch utility.

Tested this by running all of the tests in the "virtualbox" NixOS VM
test module, here is the URL to the finished evaluation on my Hydra:

https://headcounter.org/hydra/eval/380191

pkgs/top-level/all-packages.nix
@@ -12757,7 +12757,7 @@ with pkgs;
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_4_9;
+  linuxPackages = linuxPackages_4_14;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!

pkgs/applications/virtualization/virtualbox/default.nix
@@ -19,10 +19,10 @@ let
   python = python2;
   buildType = "release";
   # Manually sha256sum the extensionPack file, must be hex!
-  extpack = "005ba9211862643e2516d549e98b80942918047f1f6c55fcfe08c490dd1947bc";
-  extpackRev = "118431";
-  main = "0m6y98pvkngprw5iaswvkbbfxmzvfl8yvgi984p1866zwap77z16";
-  version = "5.2.0";
+  extpack = "9328548ca8cbc526232c0631cb5a17618c771b07665b362c1e3d89a2425bf799";
+  extpackRev = "119230";
+  main = "05y03fcp013gc500q34bj6hvx1banib41v8l3hcxknzfgwq0rarm";
+  version = "5.2.2";
 
   # See https://github.com/NixOS/nixpkgs/issues/672 for details
   extensionPack = requireFile rec {

pkgs/applications/virtualization/virtualbox/guest-additions/default.nix
@@ -19,11 +19,13 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "http://download.virtualbox.org/virtualbox/${version}/VBoxGuestAdditions_${version}.iso";
-    sha256 = "1r6dybr3pfclffk7gppf7n8gwj3ziw7pmfvbwwkdw00q9ah7h5l4";
+    sha256 = "1f0vm20qdjxqsbciwgybxqqpn609gj5dy68an8lpi1wlk93s05w3";
   };
 
   KERN_DIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
   KERN_INCL = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/include";
+
+  patchFlags = [ "-p1" "-d" "install/src/vboxguest-${version}" ];
 
   patches = [
     ./fix_kerndir.patch

pkgs/applications/virtualization/virtualbox/guest-additions/fix_kerndir.patch
@@ -1,7 +1,7 @@
-diff --git a/install/src/vboxguest-5.2.0/vboxsf/Makefile.include.header b/install/src/vboxguest-5.2.0/vboxsf/Makefile.include.header
+diff --git a/vboxsf/Makefile.include.header b/vboxsf/Makefile.include.header
 index 8df1eb4d25..5a3e5604e7 100644
---- a/install/src/vboxguest-5.2.0/vboxsf/Makefile.include.header
-+++ b/install/src/vboxguest-5.2.0/vboxsf/Makefile.include.header
+--- a/vboxsf/Makefile.include.header
++++ b/vboxsf/Makefile.include.header
 @@ -117,7 +117,6 @@ else # neq($(KERNELRELEASE),)
  endif # neq($(KERNELRELEASE),)
 
@@ -11,10 +11,10 @@ index 8df1eb4d25..5a3e5604e7 100644
   $(error Error: unable to find the headers of the Linux kernel to build against. \
            Specify KERN_VER=<version> and run Make again)
 
-diff --git a/install/src/vboxguest-5.2.0/vboxguest/Makefile.include.header b/install/src/vboxguest-5.2.0/vboxguest/Makefile.include.header
+diff --git a/vboxguest/Makefile.include.header b/vboxguest/Makefile.include.header
 index 8df1eb4d25..5a3e5604e7 100644
---- a/install/src/vboxguest-5.2.0/vboxguest/Makefile.include.header
-+++ b/install/src/vboxguest-5.2.0/vboxguest/Makefile.include.header
+--- a/vboxguest/Makefile.include.header
++++ b/vboxguest/Makefile.include.header
 @@ -117,7 +117,6 @@ else # neq($(KERNELRELEASE),)
  endif # neq($(KERNELRELEASE),)
 
@@ -24,10 +24,10 @@ index 8df1eb4d25..5a3e5604e7 100644
   $(error Error: unable to find the headers of the Linux kernel to build against. \
            Specify KERN_VER=<version> and run Make again)
 
-diff --git a/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header b/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
+diff --git a/vboxvideo/Makefile.include.header b/vboxvideo/Makefile.include.header
 index 8df1eb4d25..5a3e5604e7 100644
---- a/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
-+++ b/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
+--- a/vboxvideo/Makefile.include.header
++++ b/vboxvideo/Makefile.include.header
 @@ -117,7 +117,6 @@ else # neq($(KERNELRELEASE),)
  endif # neq($(KERNELRELEASE),)
 

pkgs/applications/virtualization/virtualbox/guest-additions/fix_kernincl.patch
@@ -1,7 +1,7 @@
-diff --git a/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header b/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
+diff --git a/vboxvideo/Makefile.include.header b/vboxvideo/Makefile.include.header
 index 8df1eb4d25..5a3e5604e7 100644
---- a/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
-+++ b/install/src/vboxguest-5.2.0/vboxvideo/Makefile.include.header
+--- a/vboxvideo/Makefile.include.header
++++ b/vboxvideo/Makefile.include.header
 @@ -122,7 +122,6 @@ ifneq ($(shell if test -d $(KERN_DIR); then echo yes; fi),yes)
            Specify KERN_VER=<version> and run Make again)
  endif

commit 9b12c4a5d33f6b7678f0981689c76672fce47163

linuxPackages: Revert accidental bump to 4.14
Regression introduced by e5c24abf3b7f12232b7d2ae08415537d54d75c91.

I've added this for my WIP tests and forgot to reset them out of the
mentioned commits.

Signed-off-by: aszlig <aszlig@nix.build>

pkgs/top-level/all-packages.nix
@@ -12757,7 +12757,7 @@ with pkgs;
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_4_14;
+  linuxPackages = linuxPackages_4_9;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!