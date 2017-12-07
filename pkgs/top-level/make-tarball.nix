adrianpk/nixpkgs

commit 6bcf1944494973029071411bb378d4262ce2a678

tarball: Only check nix files for nixpkgs references
Fixes travis build failure.

pkgs/top-level/make-tarball.nix
@@ -40,7 +40,7 @@ releaseTools.sourceTarball rec {
     echo 'abort "Illegal use of <nixpkgs> in Nixpkgs."' > $TMPDIR/barf.nix
 
     # Make sure that Nixpkgs does not use <nixpkgs>
-    if grep -r '<nixpkgs\/' pkgs; then
+    if (find pkgs -type f -name '*.nix' -print | xargs grep '<nixpkgs\/'); then
         echo "Nixpkgs is not allowed to use <nixpkgs> to refer to itself."
         exit 1
     fi

commit 0c78e73ef48bfa62c452eb3b1f052d6c8b2d2165

make-tarball.nix: Fix <nixpkgs> check
This was broken by 6bcf1944494973029071411bb378d4262ce2a678, which
caused the result of the check to be ignored (since xargs will return
a non-zero exit status if *any* grep doesn't find a match).

pkgs/top-level/make-tarball.nix
@@ -43,8 +43,10 @@ releaseTools.sourceTarball rec {
     echo 'abort "Illegal use of <nixpkgs> in Nixpkgs."' > $TMPDIR/barf.nix
 
     # Make sure that Nixpkgs does not use <nixpkgs>
-    if (find pkgs -type f -name '*.nix' -print | xargs grep '<nixpkgs\/'); then
+    badFiles=$(find pkgs -type f -name '*.nix' -print | xargs grep -l '^[^#]*<nixpkgs\/' || true)
+    if [[ -n $badFiles ]]; then
         echo "Nixpkgs is not allowed to use <nixpkgs> to refer to itself."
+        echo "The offending files: $badFiles"
         exit 1
     fi
 