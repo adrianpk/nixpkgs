adrianpk/nixpkgs

commit 60f71d83dae1b9d6bba1c477cc645f3655b21260

discord: fix "corrupt install" warnings, misc cleanup
Few things:
* Discord binary has RUNPATH not RPATH set
* patchelf uses RUNPATH if it already exits, so deps end up in RUNPATH
* RUNPATH isn't searched for plugins or transitive deps
* ..badness results

Despite this, it currently seems to work-- with the caveat
that it has a little bar on top complaining about how
"it looks like your installation is corrupt".

This fixes that warning and does some minor cleanup.

(cherry picked from commit 8753b10808338c52d59dc5f2f74fb9c089ab134c)

pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -18,6 +18,7 @@ stdenv.mkDerivation rec {
     nativeBuildInputs = [ makeWrapper ];
 
     libPath = stdenv.lib.makeLibraryPath [
+        libcxx systemd libpulseaudio
         stdenv.cc.cc alsaLib atk cairo cups dbus expat fontconfig freetype
         gdk_pixbuf glib gnome2.GConf gtk2 libnotify libX11 libXcomposite
         libXcursor libXdamage libXext libXfixes libXi libXrandr libXrender
@@ -28,15 +29,12 @@ stdenv.mkDerivation rec {
         mkdir -p $out/{bin,opt/discord,share/pixmaps}
         mv * $out/opt/discord
 
-        # Copying how adobe-reader does it,
-        # see pkgs/applications/misc/adobe-reader/builder.sh
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-                 --set-rpath "$out/opt/discord:$libPath"                                   \
+        patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
                  $out/opt/discord/Discord
 
         paxmark m $out/opt/discord/Discord
 
-        wrapProgram $out/opt/discord/Discord --prefix LD_LIBRARY_PATH : "$LD_LIBRARY_PATH:${libcxx}/lib:${systemd.lib}/lib:${libpulseaudio}/lib"
+        wrapProgram $out/opt/discord/Discord --prefix LD_LIBRARY_PATH : ${libPath}
 
         ln -s $out/opt/discord/Discord $out/bin/
         ln -s $out/opt/discord/discord.png $out/share/pixmaps

commit 75baa510a7380eeae181d030795bf9f094332525

discoard: fix eval on 17.09 broken in 60f71d83

pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
         mkdir -p $out/{bin,opt/discord,share/pixmaps}
         mv * $out/opt/discord
 
-        patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
                  $out/opt/discord/Discord
 
         paxmark m $out/opt/discord/Discord