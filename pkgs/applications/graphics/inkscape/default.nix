adrianpk/nixpkgs

commit a276d5160cc4b307d97261b419e183fb3a2214aa

inkscape: fix on Darwin
Fixes #39175

pkgs/applications/graphics/inkscape/default.nix
@@ -2,8 +2,14 @@
 , libpng, zlib, popt, boehmgc, libxml2, libxslt, glib, gtkmm2
 , glibmm, libsigcxx, lcms, boost, gettext, makeWrapper
 , gsl, python2, poppler, imagemagick, libwpg, librevenge
-, libvisio, libcdr, libexif, potrace, cmake
+, libvisio, libcdr, libexif, potrace, autoreconfHook
+, intltool
+, lib
 }:
+
+# Note that originally this Nix expression used CMake to build but
+# this led to errors on MacOS of "Too many arguments". Inkscape
+# supports autoconf and we will use this for now on.
 
 let
   python2Env = python2.withPackages(ps: with ps; [ numpy lxml ]);
@@ -17,12 +23,6 @@ stdenv.mkDerivation rec {
     sha256 = "1chng2yw8dsjxc9gf92aqv7plj11cav8ax321wmakmv5bb09cch6";
   };
 
-  unpackPhase = ''
-    cp $src ${name}.tar.bz2
-    tar xvjf ${name}.tar.bz2 > /dev/null
-    cd ${name}
-  '';
-
   postPatch = ''
     patchShebangs share/extensions
     patchShebangs fix-roff-punct
@@ -33,25 +33,26 @@ stdenv.mkDerivation rec {
       --replace '"python-interpreter", "python"' '"python-interpreter", "${python2Env}/bin/python"'
   '';
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook intltool ];
   buildInputs = [
     perl perlXMLParser libXft libpng zlib popt boehmgc
     libxml2 libxslt glib gtkmm2 glibmm libsigcxx lcms boost gettext
     makeWrapper gsl poppler imagemagick libwpg librevenge
-    libvisio libcdr libexif potrace cmake python2Env
+    libvisio libcdr libexif potrace python2Env
   ];
 
   enableParallelBuilding = true;
 
+  preConfigure = ''
+    intltoolize -f
+  '';
+
   postInstall = ''
     # Make sure PyXML modules can be found at run-time.
     rm "$out/share/icons/hicolor/icon-theme.cache"
-  '' + stdenv.lib.optionalString stdenv.isDarwin ''
-    install_name_tool -change $out/lib/libinkscape_base.dylib $out/lib/inkscape/libinkscape_base.dylib $out/bin/inkscape
-    install_name_tool -change $out/lib/libinkscape_base.dylib $out/lib/inkscape/libinkscape_base.dylib $out/bin/inkview
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     license = "GPL";
     homepage = https://www.inkscape.org;
     description = "Vector graphics editor";
@@ -62,5 +63,6 @@ stdenv.mkDerivation rec {
 
       If you want to import .eps files install ps2edit.
     '';
+    maintainers = with maintainters; [ matthewbauer ];
   };
 }

commit 0ffece80d2831f0ce90bdf8e48b96f2857e0f97e

inkscape: Fix build on NixOS
The first problem that was introduced in a276d5160cc4b307d97261b419e183f
was a linking error:

ld: cannot find -licui18n
ld: cannot find -licuuc
ld: cannot find -licudata

So I added icu to the buildInputs.

The second problem was that the interpreter wasn't patched in
share/filters, apparently this is only needed when building with
autotools:

make[3]: Entering directory '/build/inkscape-0.92.3/share/filters'
./i18n.py ./filters.svg > ./filters.svg.h
./i18n.py: /usr/bin/env: bad interpreter: No such file or directory

A similar error also occurs for share/palettes, share/patterns,
share/symbols and share/templates, so I added patching the interpreter
there as well.

Switching to autotools in Inkscape is a very bad idea, because upstream
currently still has their own autotools files in the 0.92.x tree but
master already has them removed, see this commit:

https://gitlab.com/inkscape/inkscape/commit/e471a664f923f517b68071f2e33fbb6ce070f8b7

However for the sake of trying to not break Inkscape on Darwin again,
I tried to keep the fixes minimal and not went back to CMake.

I did however mark the stuff that's unneeded for CMake, so that we can
avoid forgetting to remove that crap once we get back to CMake.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @matthewbauer

pkgs/applications/graphics/inkscape/default.nix
@@ -4,6 +4,7 @@
 , gsl, python2, poppler, imagemagick, libwpg, librevenge
 , libvisio, libcdr, libexif, potrace, autoreconfHook
 , intltool
+, icu # Not needed for building with CMake
 , lib
 }:
 
@@ -27,6 +28,13 @@ stdenv.mkDerivation rec {
     patchShebangs share/extensions
     patchShebangs fix-roff-punct
 
+    # XXX: Not needed for CMake:
+    patchShebangs share/filters
+    patchShebangs share/palettes
+    patchShebangs share/patterns
+    patchShebangs share/symbols
+    patchShebangs share/templates
+
     # Python is used at run-time to execute scripts, e.g., those from
     # the "Effects" menu.
     substituteInPlace src/extension/implementation/script.cpp \
@@ -38,7 +46,7 @@ stdenv.mkDerivation rec {
     perl perlXMLParser libXft libpng zlib popt boehmgc
     libxml2 libxslt glib gtkmm2 glibmm libsigcxx lcms boost gettext
     makeWrapper gsl poppler imagemagick libwpg librevenge
-    libvisio libcdr libexif potrace python2Env
+    libvisio libcdr libexif potrace python2Env icu
   ];
 
   enableParallelBuilding = true;