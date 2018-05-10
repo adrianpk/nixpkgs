adrianpk/nixpkgs

commit e5be49aec1b3e05180ab957af98e61b4b86eecd7

nixos/release-combined.nix: make tests consistent (#40131)
... with release-small.nix
Tests for small channel should be a subset of tests for full channel.

(cherry picked from commit f8d047da50dbc7d012a8d322b4d5e5a0b639733c)

nixos/release-combined.nix
@@ -58,6 +58,8 @@ in rec {
         nixos.ova.x86_64-linux or []
 
         #(all nixos.tests.containers)
+        (all nixos.tests.containers-imperative)
+        (all nixos.tests.containers-ipv4)
         nixos.tests.chromium.x86_64-linux or []
         (all nixos.tests.firefox)
         (all nixos.tests.firewall)
@@ -98,6 +100,7 @@ in rec {
         (all nixos.tests.misc)
         (all nixos.tests.mutableUsers)
         (all nixos.tests.nat.firewall)
+        (all nixos.tests.nat.firewall-conntrack)
         (all nixos.tests.nat.standalone)
         (all nixos.tests.networking.scripted.loopback)
         (all nixos.tests.networking.scripted.static)
@@ -112,6 +115,7 @@ in rec {
         (all nixos.tests.nfs4)
         (all nixos.tests.openssh)
         (all nixos.tests.php-pcre)
+        (all nixos.tests.predictable-interface-names)
         (all nixos.tests.printing)
         (all nixos.tests.proxy)
         (all nixos.tests.sddm.default)

commit 8fa034d035eaf46071cd0ae8ba09e55d8d421949

nixos/release-combined.nix: fix eval of tested job
bug introduced by e5be49aec1b3e05180ab957af98e61b4b86eecd7

nixos/release-combined.nix
@@ -115,7 +115,10 @@ in rec {
         (all nixos.tests.nfs4)
         (all nixos.tests.openssh)
         (all nixos.tests.php-pcre)
-        (all nixos.tests.predictable-interface-names)
+        (all nixos.tests.predictable-interface-names.vm-test-run-predictableInterfaceNames)
+        (all nixos.tests.predictable-interface-names.vm-test-run-predictableInterfaceNames-with-networkd)
+        (all nixos.tests.predictable-interface-names.vm-test-run-unpredictableInterfaceNames)
+        (all nixos.tests.predictable-interface-names.vm-test-run-unpredictableInterfaceNames-with-networkd)
         (all nixos.tests.printing)
         (all nixos.tests.proxy)
         (all nixos.tests.sddm.default)