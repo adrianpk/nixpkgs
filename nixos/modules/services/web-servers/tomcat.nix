adrianpk/nixpkgs

commit dc21f1d8d1e6afc4c32ad92b39bc175f16e00447

tomcat service: fix webapps default option (#40657)
The old package tomcat.webapps does not exist

nixos/modules/services/web-servers/tomcat.nix
@@ -109,8 +109,8 @@ in
 
       webapps = mkOption {
         type = types.listOf types.package;
-        default = [ tomcat.webapps ];
-        defaultText = "[ tomcat.webapps ]";
+        default = [ tomcat85.webapps ];
+        defaultText = "[ tomcat85.webapps ]";
         description = "List containing WAR files or directories with WAR files which are web applications to be deployed on Tomcat";
       };
 

commit 19b4c0fa860e3c083fefa7e75b1b679f656c06c5

nixos/tomcat: fix eval error introduced by #40657

nixos/modules/services/web-servers/tomcat.nix
@@ -109,8 +109,8 @@ in
 
       webapps = mkOption {
         type = types.listOf types.package;
-        default = [ tomcat85.webapps ];
-        defaultText = "[ tomcat85.webapps ]";
+        default = [ tomcat.webapps ];
+        defaultText = "[ pkgs.tomcat85.webapps ]";
         description = "List containing WAR files or directories with WAR files which are web applications to be deployed on Tomcat";
       };
 