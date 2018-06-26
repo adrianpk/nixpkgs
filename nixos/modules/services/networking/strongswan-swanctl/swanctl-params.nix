adrianpk/nixpkgs

commit 401370287a9b74fc18539fe9c0bdc7ac9df9cf22

strongswan-swanctl: adapt options to strongswan-5.6.3
This time there was only one change between 5.6.2..5.6.3:

https://github.com/strongswan/strongswan/commit/2c7a4b07045786ee493021020e2ad5bd6bb4d045

nixos/modules/services/networking/strongswan-swanctl/swanctl-params.nix
@@ -938,9 +938,12 @@ in {
         protection.
       '';
 
-      hw_offload = mkYesNoParam no ''
+      hw_offload = mkEnumParam ["yes" "no" "auto"] "no" ''
         Enable hardware offload for this CHILD_SA, if supported by the IPsec
-        implementation.
+        implementation. The value <literal>yes</literal> enforces offloading
+        and the installation will fail if it's not supported by either kernel or
+        device. The value <literal>auto<literal> enables offloading, if it's
+        supported, but the installation does not fail otherwise.
       '';
 
       start_action = mkEnumParam ["none" "trap" "start"] "none" ''

commit 8e52d534876651bd31e1b402d895ee911ae6ebff

nixos/strongswan-swanctl: Fix build of manual
Commit 401370287a9b74fc18539fe9c0bdc7ac9df9cf22 introduced a small error
where the closing tag of <literal/> was an opening tag instead.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @basvandijk, @xeji

nixos/modules/services/networking/strongswan-swanctl/swanctl-params.nix
@@ -942,7 +942,7 @@ in {
         Enable hardware offload for this CHILD_SA, if supported by the IPsec
         implementation. The value <literal>yes</literal> enforces offloading
         and the installation will fail if it's not supported by either kernel or
-        device. The value <literal>auto<literal> enables offloading, if it's
+        device. The value <literal>auto</literal> enables offloading, if it's
         supported, but the installation does not fail otherwise.
       '';
 