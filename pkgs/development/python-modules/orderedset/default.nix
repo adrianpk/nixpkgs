adrianpk/nixpkgs

commit 44a4370b1f3aa2b1b2416d8c09b8df6900f5c449

python.pkgs.orderedset init at 2.0.1

pkgs/development/python-modules/orderedset/default.nix
@@ -1,0 +1,18 @@
+{ stdenv, buildPythonPackage, fetchPypi }:
+
+buildPythonPackage rec {
+  pname = "orderedset";
+  version = "2.0.1";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "11643qr12ypxfffcminlsgl9xz751b2d0pnjl6zn8vfhxddjr57f";
+  };
+
+  meta = with stdenv.lib; {
+    description = "An Ordered Set implementation in Cython";
+    homepage = https://pypi.python.org/pypi/orderedset;
+    license = licenses.bsd3;
+    maintainers = maintainers.jtojnar;
+  };
+}

pkgs/top-level/python-packages.nix
@@ -8828,6 +8828,8 @@ in {
     };
   };
 
+  orderedset = callPackage ../development/python-modules/orderedset { };
+
   python-otr = buildPythonPackage rec {
     name = "${pname}-${version}";
     pname = "python-otr";

commit 2f9d55583a3cb44bd58553a8847c845c50a19113

python.pkgs.orderedset: fix ofborg checkMeta
broken by 44a4370b1f3aa2b1b2416d8c09b8df6900f5c449

pkgs/development/python-modules/orderedset/default.nix
@@ -13,6 +13,6 @@ buildPythonPackage rec {
     description = "An Ordered Set implementation in Cython";
     homepage = https://pypi.python.org/pypi/orderedset;
     license = licenses.bsd3;
-    maintainers = maintainers.jtojnar;
+    maintainers = [ maintainers.jtojnar ];
   };
 }