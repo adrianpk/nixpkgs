adrianpk/nixpkgs

commit b785d4813e5d0f428b9563b3cea7cc6953fc24db

qt5: 5.10 -> 5.11
This is the first time since 5.9 that we also update `qtwebkit`.
`qtwebkit` is not maintained by Qt anymore and thus, we switch to the
community port as for example arch has done. To prevent pulling in
single patches, we just stick to the latest git version.

pkgs/development/libraries/qt-5/5.11/default.nix
@@ -17,7 +17,7 @@ top-level attribute to `top-level/all-packages.nix`.
 
 {
   newScope,
-  stdenv, fetchurl, makeSetupHook, makeWrapper,
+  stdenv, fetchurl, fetchFromGitHub, makeSetupHook, makeWrapper,
   bison, cups ? null, harfbuzz, libGL, perl,
   gstreamer, gst-plugins-base, gtk3, dconf,
 
@@ -31,7 +31,7 @@ with stdenv.lib;
 
 let
 
-  qtCompatVersion = "5.10";
+  qtCompatVersion = "5.11";
 
   mirror = "http://download.qt.io";
   srcs = import ./srcs.nix { inherit fetchurl; inherit mirror; };
@@ -96,7 +96,15 @@ let
       qtwayland = callPackage ../modules/qtwayland.nix {};
       qtwebchannel = callPackage ../modules/qtwebchannel.nix {};
       qtwebengine = callPackage ../modules/qtwebengine.nix {};
-      qtwebkit = callPackage ../modules/qtwebkit.nix {};
+      qtwebkit = callPackage ../modules/qtwebkit.nix {
+        src = fetchFromGitHub {
+          owner = "annulen";
+          repo = "webkit";
+          rev = "4ce8ebc4094512b9916bfa5984065e95ac97c9d8";
+          sha256 = "05h1xnxzbf7sp3plw5dndsvpf6iigh0bi4vlj4svx0hkf1giakjf";
+        };
+        version = "5.212-alpha-01-26-2018";
+      };
       qtwebsockets = callPackage ../modules/qtwebsockets.nix {};
       qtx11extras = callPackage ../modules/qtx11extras.nix {};
       qtxmlpatterns = callPackage ../modules/qtxmlpatterns.nix {};

pkgs/development/libraries/qt-5/modules/qtwebkit.nix
@@ -1,23 +1,40 @@
-{ qtModule, stdenv, lib
-, qtbase, qtdeclarative, qtlocation, qtsensors
+{ qtModule, stdenv, lib, fetchurl
+, qtbase, qtdeclarative, qtlocation, qtsensors, qtwebchannel
 , fontconfig, gdk_pixbuf, gtk2, libwebp, libxml2, libxslt
-, sqlite, systemd, glib, gst_all_1
+, sqlite, systemd, glib, gst_all_1, cmake
 , bison2, flex, gdb, gperf, perl, pkgconfig, python2, ruby
 , darwin
 , substituteAll
 , flashplayerFix ? false
+, src ? null
+, version ? null
 }:
 
-let inherit (lib) optional optionals getLib; in
-
+let
+  inherit (lib) optional optionals getLib;
+  hyphen = stdenv.mkDerivation rec {
+    name = "hyphen-2.8.8";
+    src = fetchurl {
+      url = "http://dev-www.libreoffice.org/src/5ade6ae2a99bc1e9e57031ca88d36dad-${name}.tar.gz";
+      sha256 = "304636d4eccd81a14b6914d07b84c79ebb815288c76fe027b9ebff6ff24d5705";
+    };
+    buildInputs = [ perl ];
+  };
+in
 qtModule {
   name = "qtwebkit";
-  qtInputs = [ qtbase qtdeclarative qtlocation qtsensors ];
+  qtInputs = [ qtbase qtdeclarative qtlocation qtsensors ] ++ optionals (lib.versionAtLeast qtbase.version "5.11.0") [ qtwebchannel ];
   buildInputs = [ fontconfig libwebp libxml2 libxslt sqlite glib gst_all_1.gstreamer gst_all_1.gst-plugins-base ]
-    ++ optionals (stdenv.isDarwin) (with darwin.apple_sdk.frameworks; [ OpenGL ]);
+    ++ optionals (stdenv.isDarwin) (with darwin.apple_sdk.frameworks; [ OpenGL ])
+    ++ optionals (lib.versionAtLeast qtbase.version "5.11.0") [ hyphen ];
   nativeBuildInputs = [
     bison2 flex gdb gperf perl pkgconfig python2 ruby
-  ];
+  ] ++ optionals (lib.versionAtLeast qtbase.version "5.11.0") [ cmake ];
+
+  cmakeFlags = optionals (lib.versionAtLeast qtbase.version "5.11.0") [ "-DPORT=Qt" ];
+
+  inherit src;
+  inherit version;
 
   __impureHostDeps = optionals (stdenv.isDarwin) [
     "/usr/lib/libicucore.dylib"

pkgs/development/libraries/qt-5/qtModule.nix
@@ -8,7 +8,7 @@ args:
 
 let
   inherit (args) name;
-  version = args.version or srcs."${name}".version;
+  version = if (args.version or null) == null then srcs."${name}".version else args.version;
   src = args.src or srcs."${name}".src;
 in
 
@@ -53,7 +53,7 @@ mkDerivation (args // {
     homepage = http://www.qt.io;
     description = "A cross-platform application framework for C++";
     license = with licenses; [ fdl13 gpl2 lgpl21 lgpl3 ];
-    maintainers = with maintainers; [ qknight ttuegel periklis ];
+    maintainers = with maintainers; [ qknight ttuegel periklis bkchr ];
     platforms = platforms.unix;
   } // (args.meta or {});
 })

pkgs/development/libraries/qt-5/5.10/fetch.sh
@@ -1,2 +1,0 @@
-WGET_ARGS=( http://download.qt.io/official_releases/qt/5.10/5.10.1/submodules/ \
-            -A '*.tar.xz' )

pkgs/development/libraries/qt-5/5.10/qtwebkit.patch
@@ -1,77 +1,0 @@
-diff --git a/Source/WTF/WTF.pri b/Source/WTF/WTF.pri
-index 69e4cd1f3..3f729a75e 100644
---- a/Source/WTF/WTF.pri
-+++ b/Source/WTF/WTF.pri
-@@ -12,7 +12,7 @@ mac {
-     # Mac OS does ship libicu but not the associated header files.
-     # Therefore WebKit provides adequate header files.
-     INCLUDEPATH = $${ROOT_WEBKIT_DIR}/Source/WTF/icu $$INCLUDEPATH
--    LIBS += -licucore
-+    LIBS += /usr/lib/libicucore.dylib
- } else:!use?(wchar_unicode): {
-     win32 {
-         CONFIG(static, static|shared) {
-diff --git a/Source/WebCore/plugins/qt/PluginPackageQt.cpp b/Source/WebCore/plugins/qt/PluginPackageQt.cpp
-index a923d49aa..46772a4bb 100644
---- a/Source/WebCore/plugins/qt/PluginPackageQt.cpp
-+++ b/Source/WebCore/plugins/qt/PluginPackageQt.cpp
-@@ -136,7 +136,11 @@ static void initializeGtk(QLibrary* module = 0)
-         }
-     }
- 
-+#ifdef NIXPKGS_LIBGTK2
-+    QLibrary library(QLatin1String(NIXPKGS_LIBGTK2), 0);
-+#else
-     QLibrary library(QLatin1String("libgtk-x11-2.0"), 0);
-+#endif
-     if (library.load()) {
-         typedef void *(*gtk_init_check_ptr)(int*, char***);
-         gtk_init_check_ptr gtkInitCheck = (gtk_init_check_ptr)library.resolve("gtk_init_check");
-diff --git a/Source/WebCore/plugins/qt/PluginViewQt.cpp b/Source/WebCore/plugins/qt/PluginViewQt.cpp
-index de06a2fea..86fe39ef1 100644
---- a/Source/WebCore/plugins/qt/PluginViewQt.cpp
-+++ b/Source/WebCore/plugins/qt/PluginViewQt.cpp
-@@ -697,7 +697,11 @@ static Display *getPluginDisplay()
-     // support gdk based plugins (like flash) that use a different X connection.
-     // The code below has the same effect as this one:
-     // Display *gdkDisplay = gdk_x11_display_get_xdisplay(gdk_display_get_default());
-+#ifdef NIXPKGS_LIBGDK2
-+    QLibrary library(QLatin1String(NIXPKGS_LIBGDK2), 0);
-+#else
-     QLibrary library(QLatin1String("libgdk-x11-2.0"), 0);
-+#endif
-     if (!library.load())
-         return 0;
- 
-diff --git a/Source/WebKit2/PluginProcess/qt/PluginProcessMainQt.cpp b/Source/WebKit2/PluginProcess/qt/PluginProcessMainQt.cpp
-index 8de65216b..38f5c05e5 100644
---- a/Source/WebKit2/PluginProcess/qt/PluginProcessMainQt.cpp
-+++ b/Source/WebKit2/PluginProcess/qt/PluginProcessMainQt.cpp
-@@ -53,7 +53,11 @@ static void messageHandler(QtMsgType type, const QMessageLogContext&, const QStr
- 
- static bool initializeGtk()
- {
-+#ifdef NIXPKGS_LIBGTK2
-+    QLibrary gtkLibrary(QLatin1String(NIXPKGS_LIBGTK2), 0);
-+#else
-     QLibrary gtkLibrary(QLatin1String("libgtk-x11-2.0"), 0);
-+#endif
-     if (!gtkLibrary.load())
-         return false;
-     typedef void* (*gtk_init_ptr)(void*, void*);
-diff --git a/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp b/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
-index d734ff684..0f6ff63d1 100644
---- a/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
-+++ b/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
-@@ -64,7 +64,11 @@ static Display* getPluginDisplay()
-     // The code below has the same effect as this one:
-     // Display *gdkDisplay = gdk_x11_display_get_xdisplay(gdk_display_get_default());
- 
-+#ifdef NIXPKGS_LIBGDK2
-+    QLibrary library(QLatin1String(NIXPKGS_LIBGDK2), 0);
-+#else
-     QLibrary library(QLatin1String("libgdk-x11-2.0"), 0);
-+#endif
-     if (!library.load())
-         return 0;
- 

pkgs/development/libraries/qt-5/5.10/srcs.nix
@@ -1,341 +1,0 @@
-# DO NOT EDIT! This file is generated automatically by fetch-kde-qt.sh
-{ fetchurl, mirror }:
-
-{
-  qt3d = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qt3d-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0fvb346xhiyc0csxcvs5ifwkzs9wnza54vvsw6dvyax76qjv87q4";
-      name = "qt3d-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtactiveqt = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtactiveqt-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1r4i72i1pl9xnmcx8rr8g48fmym8mxs28pkg1nl302xi6izan8ch";
-      name = "qtactiveqt-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtandroidextras = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtandroidextras-everywhere-src-5.10.1.tar.xz";
-      sha256 = "03jmf7gw4abn5gj92b1ns0k75bbi4wycbc4bbb7c9mngzkv3nzkn";
-      name = "qtandroidextras-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtbase = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtbase-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0sd9cn3ywkjfswddcxhbfplkwk8snqld752q5laaapdakhc0wrnq";
-      name = "qtbase-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtcanvas3d = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtcanvas3d-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1h5hpjwdyp824r2ajmaqjcshra06yfzz7dp991h4kf54da79m0ny";
-      name = "qtcanvas3d-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtcharts = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtcharts-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1j8y683ainri9ma44ky6q87pjs1d794znb2wfsrbplixyxxmf3ag";
-      name = "qtcharts-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtconnectivity = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtconnectivity-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1j9ydlkwz3039yslaizz923h0qbszm4z2sqspk28wr8rd3f22a35";
-      name = "qtconnectivity-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtdatavis3d = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtdatavis3d-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0ny8j689jm2fghklzw894yc5hjqa23yf395yv17yvwi72kpiz0b3";
-      name = "qtdatavis3d-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtdeclarative = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtdeclarative-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0lwmg1qan886czzkzi4px36mqxlqiw0acrsa9kqcddg5pi8yvy9s";
-      name = "qtdeclarative-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtdoc = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtdoc-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1gl62acr346jrcfxla8vxlczf706a3md52zc0zaqbjw9rf2bgnqq";
-      name = "qtdoc-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtgamepad = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtgamepad-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1xs6adpnv5ymdmb7kcbvcc0vx2lp6wqz53pzr9amyzbgyl0jiiw5";
-      name = "qtgamepad-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtgraphicaleffects = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtgraphicaleffects-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0zw4n6s42w70r45gjg66ad0s9dpxil6wmwwrivshnqvrnlqhcfrz";
-      name = "qtgraphicaleffects-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtimageformats = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtimageformats-everywhere-src-5.10.1.tar.xz";
-      sha256 = "10x248s4a6ij18awkdskk3g7zfw6gwyj63l2jr803blyfyibl118";
-      name = "qtimageformats-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtlocation = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtlocation-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1jbjzl6gwxy4n43x6m55b86vkdpjvwyvw7xsxg4a40f72xaw0p2y";
-      name = "qtlocation-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtmacextras = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtmacextras-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1jkzizsa8nr1dp5n9p81s6ylnqfh0vqwz8l72632fg68x229jfn7";
-      name = "qtmacextras-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtmultimedia = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtmultimedia-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0j9nq8s2kzfc7zqs253pixqwd5xjv2cbqyik5lgvi9p5bq0riym1";
-      name = "qtmultimedia-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtnetworkauth = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtnetworkauth-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1p6wnma2nygqbjhrbiijjgkc8jq6lkdmq6xaxcgdh8ynkcqiq8wp";
-      name = "qtnetworkauth-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtpurchasing = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtpurchasing-everywhere-src-5.10.1.tar.xz";
-      sha256 = "090gmxy5bp44rirl5yrb1ryn97z37ri4fmmclh9z7c4dvnmr2d3j";
-      name = "qtpurchasing-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtquickcontrols = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtquickcontrols-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1dw22sk2x5bk2mix06w8dv1b05dyq0gx088aqvdz18y67ncs2cfj";
-      name = "qtquickcontrols-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtquickcontrols2 = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtquickcontrols2-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0jk01vgzc78w45q8kys1hwri3jx0hzx5vxivzmv8nh8yj0dlmijx";
-      name = "qtquickcontrols2-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtremoteobjects = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtremoteobjects-everywhere-src-5.10.1.tar.xz";
-      sha256 = "052fa27rv4b9h6qbldrq013zxb69ysgmqnxbyv9c48s2ra50pnp9";
-      name = "qtremoteobjects-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtscript = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtscript-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0bakr5xcxc6j6l78fyazh7sgcz2hwf2g8k60jb9307qzrgvqlqg0";
-      name = "qtscript-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtscxml = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtscxml-everywhere-src-5.10.1.tar.xz";
-      sha256 = "00v1n86pyw9cm320qa2fpazvkfi6czd027x60k2g2sprwh0x4hhb";
-      name = "qtscxml-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtsensors = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtsensors-everywhere-src-5.10.1.tar.xz";
-      sha256 = "15jw2r6snhwi26b4ajp7m89l89kaf1j0kgqds4wjpksk1rngf3dv";
-      name = "qtsensors-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtserialbus = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtserialbus-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0mr2j86mk1fm9z5iba3kav651231j9jv0805q8716h7ia1ws2l06";
-      name = "qtserialbus-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtserialport = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtserialport-everywhere-src-5.10.1.tar.xz";
-      sha256 = "17ja368ggl9aak64r8sqzx34j2i8zv7z60bkhncynrgipc9wpr08";
-      name = "qtserialport-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtspeech = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtspeech-everywhere-src-5.10.1.tar.xz";
-      sha256 = "07clcy3bslq4fk7xb6cqlfsz5frhmy41hzpajdqjwj9h6dvmqxw5";
-      name = "qtspeech-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtsvg = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtsvg-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0nkz3kxclk7s95fnh8xgljahk131mwb8pjllgp7n70ycmc20rq00";
-      name = "qtsvg-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qttools = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qttools-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0lwa0i6zspcrii0ak1dyxacif1fkv2vhmdp3dmp7af71bwg49spi";
-      name = "qttools-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qttranslations = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qttranslations-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1za34rvn3bhzi4mxqm9lghdhaqcxpsi6xa61dfbhpc047l788v8z";
-      name = "qttranslations-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtvirtualkeyboard = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtvirtualkeyboard-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0bnmhy0kibvyra0i15qzwc9195s8wpfhg5gk0d6rfwd5d07gj6a3";
-      name = "qtvirtualkeyboard-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwayland = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwayland-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0mk3p8pkfia3z7v7phk0qdyzd1zkfpk92c995g851p5wbqx699zm";
-      name = "qtwayland-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebchannel = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwebchannel-everywhere-src-5.10.1.tar.xz";
-      sha256 = "0r1p1ar4xh7g7lzi564hgw1ng5xkim4mkpgq29ymjlmhxjgl8b62";
-      name = "qtwebchannel-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebengine = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwebengine-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1ph1pinxn0dp44zall9gihbgziira4zpynixacm4ldd85f6lyr0j";
-      name = "qtwebengine-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebglplugin = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwebglplugin-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1hdzymwai5f6agkn4hz32aq5fjv2lqdac285fwrlk5gsff0j0yal";
-      name = "qtwebglplugin-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebsockets = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwebsockets-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1sjkml5kqigvyxrkv5b7x1cy69d69dzma40yfg72vr7zal4pkkvf";
-      name = "qtwebsockets-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebview = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwebview-everywhere-src-5.10.1.tar.xz";
-      sha256 = "110mwc423cq1bwdz0vv78cjprzy03xawxnxhfcs4138gssfj0n0b";
-      name = "qtwebview-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwinextras = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtwinextras-everywhere-src-5.10.1.tar.xz";
-      sha256 = "05w9n8kry1mg3i14i4m98gm5hf20mlwxbagbk9xk11xykbm1wh0j";
-      name = "qtwinextras-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtx11extras = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtx11extras-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1d4pvisxbcyb7ljl1lh2zg72kw44h9kk14l0xmvajb8bslgkg2n3";
-      name = "qtx11extras-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtxmlpatterns = {
-    version = "5.10.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.10/5.10.1/submodules/qtxmlpatterns-everywhere-src-5.10.1.tar.xz";
-      sha256 = "1zp9smsfdqyfiarxs7r8kfr7yq68cdn7biddwl36cybax6fgbpiw";
-      name = "qtxmlpatterns-everywhere-src-5.10.1.tar.xz";
-    };
-  };
-  qtwebkit = {
-    version = "5.9.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.9/5.9.1/submodules/qtwebkit-opensource-src-5.9.1.tar.xz";
-      sha256 = "1ksjn1vjbfhdm4y4rg08ag4krk87ahp7qcdcpwll42l0rnz61998";
-      name = "qtwebkit-opensource-src-5.9.1.tar.xz";
-    };
-  };
-  qtwebkit-examples = {
-    version = "5.9.1";
-    src = fetchurl {
-      url = "${mirror}/official_releases/qt/5.9/5.9.1/submodules/qtwebkit-examples-opensource-src-5.9.1.tar.xz";
-      sha256 = "1l2l7ycgqql6rf4gx6sjhsqjapdhvy6vxaxssax3l938nkk4vkp4";
-      name = "qtwebkit-examples-opensource-src-5.9.1.tar.xz";
-    };
-  };
-}

pkgs/development/libraries/qt-5/5.11/fetch.sh
@@ -1,0 +1,2 @@
+WGET_ARGS=( http://download.qt.io/official_releases/qt/5.11/5.11.0/submodules/ \
+            -A '*.tar.xz' )

pkgs/development/libraries/qt-5/5.11/qtbase-darwin.patch
@@ -1,16 +1,3 @@
-diff --git a/src/platformsupport/fontdatabases/mac/qfontengine_coretext.mm b/src/platformsupport/fontdatabases/mac/qfontengine_coretext.mm
-index 66baf16..89794ef 100644
---- a/src/platformsupport/fontdatabases/mac/qfontengine_coretext.mm
-+++ b/src/platformsupport/fontdatabases/mac/qfontengine_coretext.mm
-@@ -830,7 +830,7 @@ void QCoreTextFontEngine::getUnscaledGlyph(glyph_t glyph, QPainterPath *path, gl
- 
- QFixed QCoreTextFontEngine::emSquareSize() const
- {
--    return QFixed::QFixed(int(CTFontGetUnitsPerEm(ctfont)));
-+    return QFixed(int(CTFontGetUnitsPerEm(ctfont)));
- }
- 
- QFontEngine *QCoreTextFontEngine::cloneWithSize(qreal pixelSize) const
 diff --git a/src/plugins/bearer/corewlan/qcorewlanengine.mm b/src/plugins/bearer/corewlan/qcorewlanengine.mm
 index 341d3bccf2..3368234c26 100644
 --- a/src/plugins/bearer/corewlan/qcorewlanengine.mm
@@ -34,11 +21,11 @@ index 341d3bccf2..3368234c26 100644
              scanThread->interfaceName = QString::fromNSString(ifName);
              scanThread->start();
 diff --git a/src/plugins/platforms/cocoa/qcocoawindow.mm b/src/plugins/platforms/cocoa/qcocoawindow.mm
-index d1f19f2..1ac2cf1 100644
+index 54254455e4..e10f62909a 100644
 --- a/src/plugins/platforms/cocoa/qcocoawindow.mm
 +++ b/src/plugins/platforms/cocoa/qcocoawindow.mm
-@@ -1699,7 +1699,7 @@ void QCocoaWindow::applyContentBorderThickness(NSWindow *window)
-
+@@ -1674,7 +1674,7 @@ void QCocoaWindow::applyContentBorderThickness(NSWindow *window)
+ 
      if (!m_drawContentBorderGradient) {
          window.styleMask = window.styleMask & ~NSTexturedBackgroundWindowMask;
 -        [window.contentView.superview setNeedsDisplay:YES];
@@ -47,10 +34,10 @@ index d1f19f2..1ac2cf1 100644
          return;
      }
 diff --git a/src/plugins/platforms/cocoa/qnswindow.mm b/src/plugins/platforms/cocoa/qnswindow.mm
-index e846fa0..4171cd4 100644
+index cb13b7d184..0159d68dca 100644
 --- a/src/plugins/platforms/cocoa/qnswindow.mm
 +++ b/src/plugins/platforms/cocoa/qnswindow.mm
-@@ -224,7 +224,7 @@ static bool isMouseEvent(NSEvent *ev)
+@@ -231,7 +231,7 @@ static bool isMouseEvent(NSEvent *ev)
      if (pw->frameStrutEventsEnabled() && isMouseEvent(theEvent)) {
          NSPoint loc = [theEvent locationInWindow];
          NSRect windowFrame = [self convertRectFromScreen:self.frame];
@@ -59,12 +46,12 @@ index e846fa0..4171cd4 100644
          if (NSMouseInRect(loc, windowFrame, NO) && !NSMouseInRect(loc, contentFrame, NO))
              [qnsview_cast(pw->view()) handleFrameStrutMouseEvent:theEvent];
      }
-@@ -253,7 +253,7 @@ static bool isMouseEvent(NSEvent *ev)
+@@ -260,7 +260,7 @@ static bool isMouseEvent(NSEvent *ev)
  + (void)applicationActivationChanged:(NSNotification*)notification
  {
      const id sender = self;
 -    NSEnumerator<NSWindow*> *windowEnumerator = nullptr;
 +    NSEnumerator *windowEnumerator = nullptr;
      NSApplication *application = [NSApplication sharedApplication];
-
+ 
  #if QT_MACOS_PLATFORM_SDK_EQUAL_OR_ABOVE(__MAC_10_12)

commit 0c8048da389a1d116dba6906639021cc9844f889

qt5.qtwebkit: Fixes modules src being subtly broken.
b785d4813e5d0f428b9563b3cea7cc6953fc24db introduced breakage in Qt
modules for 5.6 and 5.9, especially visible is Qt Webkit.

This was manifested by having a non-sensical build log where it is using
the top-level `src` attribute as source instead of Qt Webkit's own
source.

Were it not for the `src` top-level attribute (which is a legit
package), the error would have been made obvious by passing `null` to
`src`.

This partily reverts newly introduced way `src` can be passed to a
qtModule, instead relying on extending the `srcs` attrset.

For ZHF #45960

pkgs/development/libraries/qt-5/5.11/default.nix
@@ -34,7 +34,18 @@ let
   qtCompatVersion = "5.11";
 
   mirror = "http://download.qt.io";
-  srcs = import ./srcs.nix { inherit fetchurl; inherit mirror; };
+  srcs = import ./srcs.nix { inherit fetchurl; inherit mirror; } // {
+    # Community port of the now unmaintained upstream qtwebkit.
+    qtwebkit = {
+      src = fetchFromGitHub {
+        owner = "annulen";
+        repo = "webkit";
+        rev = "4ce8ebc4094512b9916bfa5984065e95ac97c9d8";
+        sha256 = "05h1xnxzbf7sp3plw5dndsvpf6iigh0bi4vlj4svx0hkf1giakjf";
+      };
+      version = "5.212-alpha-01-26-2018";
+    };
+  };
 
   patches = {
     qtbase = [
@@ -102,15 +113,7 @@ let
       qtwayland = callPackage ../modules/qtwayland.nix {};
       qtwebchannel = callPackage ../modules/qtwebchannel.nix {};
       qtwebengine = callPackage ../modules/qtwebengine.nix {};
-      qtwebkit = callPackage ../modules/qtwebkit.nix {
-        src = fetchFromGitHub {
-          owner = "annulen";
-          repo = "webkit";
-          rev = "4ce8ebc4094512b9916bfa5984065e95ac97c9d8";
-          sha256 = "05h1xnxzbf7sp3plw5dndsvpf6iigh0bi4vlj4svx0hkf1giakjf";
-        };
-        version = "5.212-alpha-01-26-2018";
-      };
+      qtwebkit = callPackage ../modules/qtwebkit.nix {};
       qtwebsockets = callPackage ../modules/qtwebsockets.nix {};
       qtx11extras = callPackage ../modules/qtx11extras.nix {};
       qtxmlpatterns = callPackage ../modules/qtxmlpatterns.nix {};

pkgs/development/libraries/qt-5/modules/qtwebkit.nix
@@ -5,8 +5,6 @@
 , bison2, flex, gdb, gperf, perl, pkgconfig, python2, ruby
 , darwin
 , flashplayerFix ? false
-, src ? null
-, version ? null
 }:
 
 let
@@ -34,9 +32,6 @@ qtModule {
   ] ++ optionals (lib.versionAtLeast qtbase.version "5.11.0") [ cmake ];
 
   cmakeFlags = optionals (lib.versionAtLeast qtbase.version "5.11.0") [ "-DPORT=Qt" ];
-
-  inherit src;
-  inherit version;
 
   __impureHostDeps = optionals (stdenv.isDarwin) [
     "/usr/lib/libicucore.dylib"

pkgs/development/libraries/qt-5/qtModule.nix
@@ -8,7 +8,7 @@ args:
 
 let
   inherit (args) name;
-  version = if (args.version or null) == null then srcs."${name}".version else args.version;
+  version = args.version or srcs."${name}".version;
   src = args.src or srcs."${name}".src;
 in
 