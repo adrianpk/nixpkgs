adrianpk/nixpkgs

commit cfc43138cb2d906b9b24563a4487949efb106532

openblas: fix on darwin
The openblas script relies on CC=clang for succesfully compiling on
Darwin systems. This seems like bad behavior, but we get things like
https://hydra.nixos.org/build/87657058 otherwise.

Fixes #55536.

pkgs/development/libraries/science/math/openblas/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, fetchpatch, gfortran, perl, which, config, coreutils
+{ stdenv, fetchFromGitHub, fetchpatch, gfortran, perl, which, config
 # Most packages depending on openblas expect integer width to match
 # pointer width, but some expect to use 32-bit integers always
 # (for compatibility with reference BLAS).
@@ -102,24 +102,18 @@ stdenv.mkDerivation rec {
     "relro" "bindnow"
   ];
 
-  nativeBuildInputs = [
-    perl
-    which
-    buildPackages.gfortran
-    buildPackages.stdenv.cc
-  ] ++ optionals stdenv.isDarwin [
-    coreutils
-  ];
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+  nativeBuildInputs = [ perl which gfortran ];
 
   makeFlags = mapAttrsToList (var: val: "${var}=${toString val}") (config // {
     FC = "${stdenv.cc.targetPrefix}gfortran";
-    CC = "${stdenv.cc.targetPrefix}cc";
+    CC = "${stdenv.cc.targetPrefix}${if stdenv.cc.isClang then "clang" else "cc"}";
     PREFIX = placeholder "out";
     NUM_THREADS = 64;
     INTERFACE64 = blas64;
     NO_STATIC = true;
     CROSS = stdenv.hostPlatform != stdenv.buildPlatform;
-    HOSTCC = "${buildPackages.stdenv.cc.targetPrefix}cc";
+    HOSTCC = "cc";
     NO_BINARY_MODE = stdenv.hostPlatform != stdenv.buildPlatform;
   });
 

commit 22cda798edbd5393222d32e9cb3eec3a48641697

openblas: fix on linux
This reverse changes made cfc4313 which broke builds on linux. Still
not sure why this broke Linux, but we can investigate it later.
“buildPackages.stdenv.cc” shouldn’t be put in nativeBuildInputs in
general. Either way, this restores hashes to before that commit.

thanks to @jethrokuan

pkgs/development/libraries/science/math/openblas/default.nix
@@ -102,8 +102,12 @@ stdenv.mkDerivation rec {
     "relro" "bindnow"
   ];
 
-  depsBuildBuild = [ buildPackages.stdenv.cc ];
-  nativeBuildInputs = [ perl which gfortran ];
+  nativeBuildInputs = [
+    perl
+    which
+    buildPackages.gfortran
+    buildPackages.stdenv.cc
+  ];
 
   makeFlags = mapAttrsToList (var: val: "${var}=${toString val}") (config // {
     FC = "${stdenv.cc.targetPrefix}gfortran";